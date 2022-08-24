37a47e6d300f7b8216bd3219fe92c7675fb5a375
Shilajit <shilajit2014paul@gmail.com>
2022-08-24 23:33:21 +0530

    Navbar Footer

--- urbancompany/src/App.js
+++ urbancompany/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Components/shilajit/Navbar';
+import Footer from './Components/shilajit/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <Navbar/> */}
+      <Footer/>
     </div>
   );
 }
