1e326e2d800b005425471afeacd117e64441f83e
tarunaNangla <tarunanangla131@gmail.com>
2022-08-24 23:30:52 +0530

    done blog

--- urbancompany/src/App.js
+++ urbancompany/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Final from './Pages/Taruna/Final';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <Final/> */}
     </div>
   );
 }
---
+++ urbancompany/src/Pages/Taruna/Final.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Blog from './Blog'
+import Bolgnav from './Bolgnav'
+
+const Final = () => {
+  return (
+    <div>
+      <Blog/>
+      <Bolgnav/>
+    </div>
+  )
+}
+
+export default Final
