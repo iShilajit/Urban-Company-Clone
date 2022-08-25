b6bd76fc78288b67b738410ae06a7b81c163f9ac
tarunaNangla <tarunanangla131@gmail.com>
2022-08-25 23:11:17 +0530

    day3

---
+++ urbancompany/src/Pages/Taruna/Wedding.jsx
@@ -0,0 +1,203 @@
+
+import React from 'react'
+import  styles from "./Blognav.module.css"
+import { Heading, Box, Flex, Image, Text } from '@chakra-ui/react'
+const Wedding = () => {
+  return (
+    <Box pl="100px" pr="100px"  h="1400px" textAlign={"left"} >
+     <Flex gap="30px">
+      {/* Box1  */}
+      <Box w="33.5%" >
+      <Heading as='h1'size='lg' mt="5px">
+      Weddings
+       </Heading>
+       <Box mt="20px" mb="30px">
+       <hr  className={styles.hrr} />
+     </Box>
+     <Box>
+      <Image src="https://de927adv5b23k.cloudfront.net/wp-content/uploads/2019/08/04145906/Fotowalle2-1.jpg-min-1.jpg"></Image>
+     </Box>
+     <Box pt="45px">
+          <Text fontSize="13px" color={"gray"}>AUGUST 19, 2019</Text>
+     </Box>
+    <Box pt="15px">
+          <Heading as="h2"size='xl'>20 Wedding Saree Ideas To Inspire You!</Heading>
+    </Box>
+    <Text mt="15px" fontSize="18px" w="450px" >
+    That, there is no match to the grace and elegance of an Indian saree, is old news. No wonder generations of women opted for the timeless nine yards on their wedding days…
+                    </Text>
+                    <Box mt="50px">
+            <Box>
+                 
+                 <Flex gap="20px">
+                    <Box>
+                        <Image w="100px" pt="10px" h="100px" src="https://de927adv5b23k.cloudfront.net/wp-content/uploads/2019/07/04140709/New-latest-green-floral-Mandap-decor-by-Bhoomi-wedding-planners-1-min-900x600.jpg"></Image>
+                    </Box>
+                    
+                    <Box>
+                        
+                        <Box mt="7px">
+                            <Text fontSize="12px" >7 Mandap Decor Designs Based On The Foliage & Greenery Trend</Text>
+                        </Box>
+                    </Box>
+                
+                 </Flex>
+                <Box pt="20px" pb="20px">
+                    <hr />
+                </Box>
+
+            </Box>
+            <Box>
+            <Flex gap="20px">
+                    <Box>
+                        <Image w="100px" pt="10px" h="100px" src="https://de927adv5b23k.cloudfront.net/wp-content/uploads/2019/07/04141033/Super-minimal-bridal-makeup-with-red-lehenga-min-900x903.jpg"></Image>
+                    </Box>
+                    <Box>
+                        
+                        <Box mt="7px">
+                            <Text fontSize="12px" >5 Brides Who Chose To Go Super Minimal With Their Makeup & Looked Fabulous!</Text>
+                        </Box>
+                    </Box>
+                 </Flex>
+                 <Box pt="20px" pb="20px">
+                    <hr />
+                </Box>
+            </Box>
+            <Box>
+            <Flex gap="20px">
+                    <Box>
+                        <Image w="100px" h="100px" pt="10px" src="https://de927adv5b23k.cloudfront.net/wp-content/uploads/2019/05/30140559/Indoor-stage-House-on-the-clouds-wedding-stage-decor-ideas-for-Indian-weddings-900x1024.jpg"></Image>
+                    </Box>
+                    <Box>
+                        
+                        <Box mt="7px">
+                            <Text fontSize="12px" >Stage Decor Ideas for Smaller Functions (Engagement, Cocktail, Sangeet!)</Text>
+                        </Box>
+                    </Box>
+                 </Flex>
+                 <Box pt="20px" pb="20px">
+                    <hr />
+                </Box>
+            </Box>
+
+            <Box>
+            <Flex gap="20px">
+                    <Box>
+                        <Image w="100px" h="100px" pt="10px" src="https://de927adv5b23k.cloudfront.net/wp-content/uploads/2020/08/24091358/kama-480x480.png"></Image>
+                    </Box>
+                    <Box>
+                      
+                        <Box mt="7px">
+                            <Text fontSize="12px" >14 Best Stage Decoration Ideas For Indian Weddings!</Text>
+                        </Box>
+                    </Box>
+                 </Flex>
+                 <Box pt="20px" pb="20px">
+                    <hr />
+                </Box>
+            </Box>
+
+            </Box>
+    </Box>
+      
+      {/* Box 2  */}
+      <Box  w="33.5%">
+      <Heading as='h1'size='lg' mt="5px">
+      Interiors
+       </Heading>
+       <Box mt="20px" mb="30px">
+       <hr  className={styles.hrr} />
+     </Box>
+     <Box>
+      <Image src="https://de927adv5b23k.cloudfront.net/wp-content/uploads/2020/09/29170111/Untitled-design-1-2.jpg"></Image>
+     </Box>
+     <Box pt="45px">
+                    <Text fontSize="13px" color={"gray"}>SEPTEMBER 29, 2020</Text>
+                </Box>
+                <Box pt="15px">
+                        <Heading as="h2"size='xl'>Wallpapers and Stencils Are a Great Way to Go, Here’s Why</Heading>
+                    </Box>
+
+                    <Text mt="15px" fontSize="18px" w="450px" >
+                    What’s a cost-effective but great way to get statement walls? You guessed it. Both wallpapers and stencils for walls are great to break the monotony of solid coloured walls. Have doubts? Let…
+                    </Text>
+                    <Box mt="50px">
+            <Box>
+                 
+                 <Flex gap="20px">
+                    <Box>
+                        <Image w="100px" pt="10px" h="100px" src="https://de927adv5b23k.cloudfront.net/wp-content/uploads/2020/09/17134553/Untitled-design-2-1-300x224.jpg"></Image>
+                    </Box>
+                    
+                    <Box>
+                       
+                        <Box mt="7px">
+                            <Text fontSize="12px" >All You Need to Know About Enamel Painting</Text>
+                        </Box>
+                    </Box>
+                
+                 </Flex>
+                <Box pt="20px" pb="20px">
+                    <hr />
+                </Box>
+
+            </Box>
+            <Box>
+            <Flex gap="20px">
+                    <Box>
+                        <Image w="100px" pt="10px" h="100px" src="https://de927adv5b23k.cloudfront.net/wp-content/uploads/2020/09/14153741/Untitled-design-768x432.jpg"></Image>
+                    </Box>
+                    <Box>
+                       
+                        <Box mt="7px">
+                            <Text fontSize="12px" >Wood Polishing: All You Need to Know</Text>
+                        </Box>
+                    </Box>
+                 </Flex>
+                 <Box pt="20px" pb="20px">
+                    <hr />
+                </Box>
+            </Box>
+            <Box>
+            <Flex gap="20px">
+                    <Box >
+                        <Image w="100px" h="100px" pt="10px" src="https://de927adv5b23k.cloudfront.net/wp-content/uploads/2020/09/08173613/waterproof-300x224.jpg"></Image>
+                    </Box>
+                    <Box>
+                       
+                        <Box mt="7px">
+                            <Text fontSize="12px" >Textured Walls: Everything You Need to Know</Text>
+                        </Box>
+                    </Box>
+                 </Flex>
+                 <Box pt="20px" pb="20px">
+                    <hr />
+                </Box>
+            </Box>
+
+            <Box>
+            <Flex gap="20px">
+                    <Box>
+                        <Image w="100px" h="100px" pt="10px" src="https://de927adv5b23k.cloudfront.net/wp-content/uploads/2020/09/08175019/waterproofsx-768x432.jpg"></Image>
+                    </Box>
+                    <Box>
+                       
+                        <Box mt="7px">
+                            <Text fontSize="12px" >Waterproofing Your Walls: All You Need to Know</Text>
+                        </Box>
+                    </Box>
+                 </Flex>
+                 <Box pt="20px" pb="20px">
+                    <hr />
+                </Box>
+            </Box>
+
+            </Box>
+      </Box>
+     </Flex> 
+    
+    </Box>
+  )
+}
+
+export default Wedding
